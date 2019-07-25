76d4515b93740ddead113229ed16a1687f201419
sageexplorer <srimal@campusexplorer.com>
2019-07-24 19:04:00 -0700

    delete method

--- backend/src/lambda/http/deleteTodo.ts
+++ backend/src/lambda/http/deleteTodo.ts
@@ -1,13 +1,47 @@
 import 'source-map-support/register'
 
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import * as AWS from 'aws-sdk'
+
+
+const docClient = new AWS.DynamoDB.DocumentClient()
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     const todoId = event.pathParameters.todoId
+
+
+    const new_result = await docClient.query({
+            TableName: 'TodosTable',
+            IndexName: 'todoid',
+            KeyConditionExpression: 'todoId = :todoId',
+            ExpressionAttributeValues: {
+           ':todoId': todoId
+    },
+    }).promise()
     
-    const parseBody = (event["queryStringParameters"])
+    const items = await new_result.Items 
+    const name = items[0].name
+    const due  = items[0].dueDate
+    
+        const result =  await docClient.delete({
+            TableName: 'TodosTable',
+            Key :{
+                "name": name,
+                "dueDate": due
+            },
+        
+        }).promise()
+        const new_items =  result
 
-
-   return undefined
+    return {
+        statusCode: 200,
+        headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*'
+        },
+        body: JSON.stringify({
+          items 
+        })
+      }
+    
   }
-
