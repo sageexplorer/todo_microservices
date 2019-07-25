804a5e213c99a75f66357b86863c795af93ac52b
sageexplorer <srimal@campusexplorer.com>
2019-07-25 16:59:02 -0700

    return presigned url

--- backend/src/lambda/http/generateUploadUrl.ts
+++ backend/src/lambda/http/generateUploadUrl.ts
@@ -1,10 +1,39 @@
 import 'source-map-support/register'
+import * as AWS  from 'aws-sdk'
+import * as uuid from 'uuid'
+import * as AWSXRay from 'aws-xray-sdk'
+import { cors } from 'middy/middlewares'
 
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
 
+const XAWS = AWSXRay.captureAWS(AWS)
+
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const todoId = event.pathParameters.todoId
+ 
 
-  // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
-  return undefined
+  const imageId = uuid.v4()
+  const s3 = new XAWS.S3({
+    signatureVersion: 'v4'
+  })
+  const url = s3.getSignedUrl('putObject', {
+    Bucket: 'serverlesstodoapp',
+    Key: todoId,
+    ContentType: 'image/png',
+    Expires: 30000
+  })
+
+  console.log("MY SIGNED URL IS", url)
+  return {
+    statusCode: 201,
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*'
+     },
+    body: JSON.stringify({
+      uploadUrl: url
+    })
+  }
+
+   
 }
