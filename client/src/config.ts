f70eeba8d655851d27466dad545b125a8db2780a
sageexplorer <srimal@campusexplorer.com>
2019-07-27 11:57:19 -0700

    Update config.ts

---
+++ client/src/config.ts
@@ -0,0 +1,9 @@
+const apiId = 'can1ouhprj'
+export const apiEndpoint = `https://can1ouhprj.execute-api.us-east-1.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: '',
+  clientId: '',
+  callbackUrl: 'http://localhost:3001/callback'
+}
+
