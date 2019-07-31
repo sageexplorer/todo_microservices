a3bfecce5be5a49ef1d510dd464f2bd061df18a5
sageexplorer <srimal@campusexplorer.com>
2019-07-31 13:34:48 -0700

    Update updateTodo.ts

--- backend/src/lambda/http/updateTodo.ts
+++ backend/src/lambda/http/updateTodo.ts
@@ -1,44 +1,44 @@
-import 'source-map-support/register'
-import * as AWS from 'aws-sdk'
-const docClient = new AWS.DynamoDB.DocumentClient()
+import "source-map-support/register";
+import * as AWS from "aws-sdk";
+const docClient = new AWS.DynamoDB.DocumentClient();
 
-import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
+import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from "aws-lambda";
 
-import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest'
+import { UpdateTodoRequest } from "../../requests/UpdateTodoRequest";
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  const updatedTodo: UpdateTodoRequest = JSON.parse(event.body)
-  const todoId = event.pathParameters.todoId
+  const updatedTodo: UpdateTodoRequest = JSON.parse(event.body);
 
-    const name = updatedTodo["name"]
-    const dueDate  = updatedTodo["dueDate"]
-    const done = updatedTodo["done"]
-    
-  
-    const result = await docClient.update({
-     TableName: 'TodosTable',
-     Key: {
-        "name": name,
-        "dueDate": dueDate
-    },
-    UpdateExpression:  "set done = :d",
-    ExpressionAttributeValues:{
-       ":d": done,
-   
-   },
-       
-   }).promise()
+  const name = updatedTodo["name"];
+  const dueDate = updatedTodo["dueDate"];
+  const done = updatedTodo["done"];
 
-  const items = await result 
+  const items = await updateTodo(name, dueDate, done);
   return {
     statusCode: 200,
     headers: {
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*'
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*"
     },
     body: JSON.stringify({
-     items: items 
+      items: items
     })
-  }
-
+  };
+};
+async function updateTodo(name: string, dueDate: string, done: boolean) {
+  const result = await docClient
+    .update({
+      TableName: "TodosTable",
+      Key: {
+        name: name,
+        dueDate: dueDate
+      },
+      UpdateExpression: "set done = :d",
+      ExpressionAttributeValues: {
+        ":d": done
+      }
+    })
+    .promise();
+  return result;
 }
+
