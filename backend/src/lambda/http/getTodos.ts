a12e5a6b5c83ef45dddee87122a1442355121df5
sageexplorer <srimal@campusexplorer.com>
2019-07-31 13:34:10 -0700

    Update getTodos.ts

--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -1,36 +1,38 @@
-import 'source-map-support/register'
-import * as AWS from 'aws-sdk'
+import "source-map-support/register";
+import * as AWS from "aws-sdk";
 
-import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
-import { JsonWebTokenError } from 'jsonwebtoken';
-import {getUserId} from '../utils'
+import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from "aws-lambda";
+import { JsonWebTokenError } from "jsonwebtoken";
+import { getUserId } from "../utils";
 
-
-const docClient = new AWS.DynamoDB.DocumentClient()
+const docClient = new AWS.DynamoDB.DocumentClient();
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  // TODO: Get all TODO items for a current user
-  const userId = getUserId(event)
-  const result = await docClient.query({
-    TableName: 'TodosTable',
-    IndexName: 'userid',
-    KeyConditionExpression: 'userId = :userId',
-    ExpressionAttributeValues: {
-      ':userId': userId
-  }
-  }).promise()
-  const items = await result.Items
-  //const newitems = await items.filter(i => i.userId == getUserId)
-  console.log(event)
-  console.log("MY USER ID IS", userId)
+  const userId = getUserId(event);
+  const items = await getTodo(userId);
   return {
     statusCode: 200,
     headers: {
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*'
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*"
     },
     body: JSON.stringify({
-      items
+      items: items.Items
     })
-  }
+  };
+};
+
+async function getTodo(userId: string) {
+  const result = await docClient
+    .query({
+      TableName: "TodosTable",
+      IndexName: "userid",
+      KeyConditionExpression: "userId = :userId",
+      ExpressionAttributeValues: {
+        ":userId": userId
+      }
+    })
+    .promise();
+
+  return result;
 }
