0ffbed72990d4e4091da84c6af0b600855043919
sageexplorer <srimal@campusexplorer.com>
2019-07-31 13:33:23 -0700

    Update deleteTodo.ts

--- backend/src/lambda/http/deleteTodo.ts
+++ backend/src/lambda/http/deleteTodo.ts
@@ -1,47 +1,52 @@
-import 'source-map-support/register'
+import "source-map-support/register";
 
-import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
-import * as AWS from 'aws-sdk'
+import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from "aws-lambda";
+import * as AWS from "aws-sdk";
 
-
-const docClient = new AWS.DynamoDB.DocumentClient()
+const docClient = new AWS.DynamoDB.DocumentClient();
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    const todoId = event.pathParameters.todoId
+  const todoId = event.pathParameters.todoId;
 
+  const items = await deleteTodo(todoId);
 
-    const new_result = await docClient.query({
-            TableName: 'TodosTable',
-            IndexName: 'todoid',
-            KeyConditionExpression: 'todoId = :todoId',
-            ExpressionAttributeValues: {
-           ':todoId': todoId
+  return {
+    statusCode: 200,
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*"
     },
-    }).promise()
-    
-    const items = await new_result.Items 
-    const name = items[0].name
-    const due  = items[0].dueDate
-    
-        const result =  await docClient.delete({
-            TableName: 'TodosTable',
-            Key :{
-                "name": name,
-                "dueDate": due
-            },
-        
-        }).promise()
-        const new_items =  result
+    body: JSON.stringify({
+      items
+    })
+  };
+};
 
-    return {
-        statusCode: 200,
-        headers: {
-          'Content-Type': 'application/json',
-          'Access-Control-Allow-Origin': '*'
-        },
-        body: JSON.stringify({
-          items 
-        })
+async function deleteTodo(todoId: string) {
+  const new_result = await docClient
+    .query({
+      TableName: "TodosTable",
+      IndexName: "todoid",
+      KeyConditionExpression: "todoId = :todoId",
+      ExpressionAttributeValues: {
+        ":todoId": todoId
       }
-    
-  }
+    })
+    .promise();
+
+  const items = await new_result.Items;
+  const name = items[0].name;
+  const due = items[0].dueDate;
+
+  const result = await docClient
+    .delete({
+      TableName: "TodosTable",
+      Key: {
+        name: name,
+        dueDate: due
+      }
+    })
+    .promise();
+
+  return result;
+}
